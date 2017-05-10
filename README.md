4e9a0ec87d64c549f27d88ac1fb5f8121eda1028
Sayan 'Riju' Chakrabarti <s26c.sayan@gmail.com>
2017-05-10 20:25:22 +0530

    Init...

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # realtime-chat-express-faye
+Read the blog post at http://sayanriju.github.io/blog/Realtime-Chat-with-Express-and-Faye-Bayeux-Protocol
---
+++ public/javascripts/script.js
@@ -0,0 +1,24 @@
+// Create the Faye client. 
+// (All you need is the mount point for the Faye server you want to connect to)
+ var client = new Faye.Client('http://localhost:3000/faye')
+ 
+$(document).ready(function () {
+
+  $("#submit-btn").click(function (evt) {
+    evt.preventDefault()
+    var newMessage = $("#new-message").val()
+    // We PUBLISH our new message to a CHANNEL (/messages)
+    // which is dynamically "created" *in situ*, if required
+    client.publish('/messages', {
+      text: newMessage
+    })
+  })
+    
+  // Now we setup the client to SUBSCRIBE (listen in)
+  // for messages coming into the same CHANNEL (/messages)
+  // (Note that, the client which publishes the message itself is also subscribed)
+  client.subscribe('/messages', function(newMessage) {
+    $("#messages-list").append("<li>" + newMessage.text + "</li>")
+  });
+    
+})
